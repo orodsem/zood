5b72e370ce3f88694a837f1ca888ed0604e17822
Ramon Christopher Morales <ramonchristophermorales@gmail.com>
2019-11-24 00:38:34 +0800

    ramon - init health provider, andjustments in nav

---
+++ src/AppBundle/Controller/HealthProviderController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\View\View;
+use ProviderBundle\Entity\Provider;
+use ProviderBundle\Repository\ProviderRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class HealthProviderController extends Controller
+{
+    /**
+     * @Route("/health-provider", name="healthProvider.index")
+     */
+    public function indexAction(Request $request)
+    {
+        $html = ($this->render('health-provider/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]));
+
+        echo $html->getContent();
+        exit;
+    }
+
+    /**
+     * @Route("/health-provider/register", name="healthProvider.register")
+     */
+    public function registerAction(Request $request)
+    {
+        $html = ($this->render('health-provider/register.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]));
+
+        echo $html->getContent();
+        exit;
+    }
+
+}
---
+++ web/js/health-provider.js
@@ -0,0 +1,16 @@
+/**
+ * HealthProvider class
+ */
+class HealthProvider {
+
+    constructor() {
+        this.isLoading = false;
+    } // constructor()
+
+    init() {
+
+        var _this = this;
+
+
+    }
+} // class Login
